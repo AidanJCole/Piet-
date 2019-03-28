0f18ee3836a6722c3cb25b5e3530743f46a747a0
Aidan Cole <aidanjcole@gmail.com>
2019-03-27 21:38:56 -0400

    Added method and test file

--- interpreter.py
+++ interpreter.py
@@ -1,3 +1,13 @@
 from PIL import Image
 
 
+
+def getStack(stack, depth):
+    temp = stack
+    for x in range(depth):
+        if isinstance(temp[0], list):
+            temp = temp[0]
+        else:
+            return
+
+    return temp
