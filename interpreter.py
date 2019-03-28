c331c8cbc9adb5f7714011609279f45391b894b9
Aidan Cole <aidanjcole@gmail.com>
2019-03-27 22:00:03 -0400

    Added basic methods + passing tests

--- interpreter.py
+++ interpreter.py
@@ -1,13 +1,23 @@
 from PIL import Image
 
 
-
+# Piet++ often travers stacks of stacks. This function returns the stack at a given depth.
 def getStack(stack, depth):
     temp = stack
     for x in range(depth):
-        if isinstance(temp[0], list):
-            temp = temp[0]
+        if len(temp) > 0 and isinstance(temp[-1], list):
+            temp = temp[-1]
         else:
             return
 
     return temp
+
+# Function to determine maximum depth of recursive stacks.
+def getDepth(stack):
+    depth = 0
+    temp = stack
+    while len(temp) > 0 and isinstance(temp[-1], list):
+        depth+=1
+        temp = temp[-1]
+
+    return depth
--- interpreter_test.py
+++ interpreter_test.py
@@ -1,1 +1,12 @@
+from interpreter import *
 
+def testGetStack():
+    assert getStack([], 0) == []
+    assert getStack([1,[2]], 1) == [2]
+    assert getStack([[2],1], 1) == None
+
+def testGetDepth():
+    assert getDepth([]) == 0
+    assert getDepth([1]) == 0
+    assert getDepth([[],0]) == 0
+    assert getDepth([0,[]]) == 1
